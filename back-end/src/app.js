52c188a47c1160a300800eeb376af7ac45122762
Pauline Kraus <kraus.pauline1@gmail.com>
2022-11-16 16:47:31 +0100

    link with DB effective

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -3,15 +3,36 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const app = express();
+
 const database = require("./database");
-const { application } = require("express");
 
 const BACKEND_PORT = process.env.BACKEND_PORT ?? 5005;
+app.use(cors());
 
-application.get("/", (req, res) => {
-  res.status(200).send("Welcome to our API");
+app.get("/", (req, res) => {
+  let output = "";
+  let error = false;
+
+  database
+    .getConnection()
+    .then(() => {
+      output += "Database connection working well.";
+    })
+    .catch((err) => {
+      error = true;
+      console.log(err);
+      output += "Database connection malfunctining.";
+    })
+    .finally(() => {
+      if (error) {
+        res.status(500).send(output);
+      } else {
+        res.status(200).send("Welcome to our API" + output);
+      }
+    });
 });
 
-application.listen(BACKEND_PORT, () => {
+app.listen(BACKEND_PORT, () => {
   console.log("Listening on port", BACKEND_PORT);
 });
