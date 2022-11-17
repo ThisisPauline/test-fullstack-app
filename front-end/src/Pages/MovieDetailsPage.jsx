8285bce4c20478c69f55cf0cb605f530575d6fa6
Pauline Kraus <kraus.pauline1@gmail.com>
2022-11-17 16:21:07 +0100

    modifying DB from FE

--- front-end/src/Pages/MovieDetailsPage.jsx
+++ front-end/src/Pages/MovieDetailsPage.jsx
@@ -3,19 +3,20 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
+import styles from "./MovieDetailsPage.module.css";
 
 const MovieDetailsPage = () => {
-  const [movie, setMovie] = useState([]);
+  const [movie, setMovie] = useState(undefined);
 
   const { id } = useParams();
 
-  const backendURL = "http://localhost:5005";
+  const uri = `http://localhost:5005/movies/${id}`;
 
   useEffect(() => {
     const source = axios.CancelToken.source();
 
     axios
-      .get(`${backendURL}/movies/${id}`, { cancelToken: source.token })
+      .get(uri, { cancelToken: source.token })
       .then((response) => response.data)
       .then((data) => setMovie(data[0]));
 
@@ -26,7 +27,51 @@ const MovieDetailsPage = () => {
 
   console.log(movie);
 
-  return <div>{movie.title}</div>;
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios
+      .put(uri, movie)
+      .then((response) => response.data)
+      .then((data) => console.log(data));
+  }
+
+  return (
+    <div className={styles.movieContainer}>
+      {movie != null && (
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={movie.title}
+            onChange={(e) =>
+              setMovie({ ...movie, ...{ title: e.target.value } })
+            }
+          />
+          <input
+            type="text"
+            value={movie.genre}
+            onChange={(e) =>
+              setMovie({ ...movie, ...{ genre: e.target.value } })
+            }
+          />
+          <input
+            type="text"
+            value={movie.year}
+            onChange={(e) =>
+              setMovie({ ...movie, ...{ year: e.target.value } })
+            }
+          />
+          <input
+            type="text"
+            value={movie.rate}
+            onChange={(e) =>
+              setMovie({ ...movie, ...{ rate: e.target.value } })
+            }
+          />
+          <button type="submit">Save changes</button>
+        </form>
+      )}
+    </div>
+  );
 };
 
 export default MovieDetailsPage;
