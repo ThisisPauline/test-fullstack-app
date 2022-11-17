c26f744f5e717b34c19c393eba4c592c4863062f
Pauline Kraus <kraus.pauline1@gmail.com>
2022-11-17 11:49:17 +0100

    data from backend to frontend cors and axios

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,18 +1,25 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import reactLogo from "./assets/react.svg";
 import "./App.css";
 import MovieList from "./components/MovieList";
-import { useState } from "react";
 
 function App() {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Deadpool",
-      genre: "action",
-      year: 1234,
-      rate: 4.8,
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+
+  const backendURL = "http://localhost:5005";
+
+  useEffect(() => {
+    const source = axios.CancelToken.source();
+    axios
+      .get(`${backendURL}/movies`, { cancelToken: source.token })
+      .then((response) => response.data)
+      .then((data) => setMovies(data));
+
+    return () => {
+      source.cancel("Component got unmounted");
+    };
+  }, []);
 
   return (
     <div className="App">
